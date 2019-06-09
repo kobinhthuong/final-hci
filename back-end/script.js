0b6250e753ee7093ef7ae2d7fca2233107bb93b8
MaiVanTruong123 <maitruong158@gmail.com>
2019-06-09 23:28:16 +0700

    truong

--- back-end/script.js
+++ back-end/script.js
@@ -58,11 +58,7 @@ function updateRecord(formData) {
     selectedRow.cells[3].innerHTML = formData.Post;
 }
 
-function onDelete(
-
-
-
-) {
+function onDelete() {
     if (confirm('Are you sure to delete this record ?')) {
         row = td.parentElement.parentElement;
         document.getElementById("employeeList").deleteRow(row.rowIndex);
